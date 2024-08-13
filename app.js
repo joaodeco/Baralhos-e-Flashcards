7005e37acf1b61b3ae2744218ee473f7f4ff32c7
joaodeco <murilomergner@gmail.com>
2024-08-13 15:24:19 -0300

    Baixando prompt-sync

---
+++ app.js
@@ -0,0 +1,11 @@
+const baralhos = require ('./data')
+const flashcards = require ('./data')
+const prompt = require('prompt-sync')()
+
+function menuPrincipal(){
+    console.log('Esses são as nossas opções, escolha uma delas para poder gerenciar seus baralhos ou flashcards!!')
+
+    console.log(`
+    
+    `)
+}
---
+++ data.js
@@ -0,0 +1,11 @@
+let baralhos = {
+    id: 1, titulo: 'Murilo',
+    id: 2, titulo: 'Juliano'
+}
+
+let flashcards = {
+    id: 1, pergunta: 'Porque a galinha atravessou a rua ?', resposta: 'Pra chegar do outro lado!!',
+    id: 2, pergunta: 'Qual o animal mais chique do mundo ?', resposta: 'O porco, pois ele vive no chiqueiro!!'
+}
+
+module.exports = baralhos, flashcards
